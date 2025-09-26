3dd488d4d31952d859793e1b3641ef027ea23678
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-09-25 23:20:32 -0300

    ok

--- segundo.py
+++ segundo.py
@@ -47,5 +47,9 @@
 # print(f"Com R${r:.2f} você consegue comprar U${r/d:.2f}")
 
 # Pintar paredes
+l=float(input("Digite a largura: "));
+at=float(input("Digite a altura: "));
+area=l*at;
 
-
+print(f"Sua parede tem a dimensão {l:.2f}X{at:.2f} e sua área é de {area:.2f}m²");
+print(f"Para pintar essa parede, você precisará de {area/2}L de tinta");
