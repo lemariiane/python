17a0cf35145da720b12d3c8a983616ef70b42292
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-10-24 23:17:27 -0300

    Update analizando_dados.py

--- analizando_dados.py
+++ analizando_dados.py
@@ -1,24 +1,35 @@
 nomes=[]
 idades=[]
+sexos=[]
 mais_velho=0
+nome_homem=' ';
+f_menor=0;
 
-for i in range(1,3):
+for i in range(1,5):
     print(f'-------{i}° PESSOA-------');
 
     nome=str(input('Digitte o nome: '));
     nomes.append(nome);
     idade=int(input('Digite a idade: '));
     idades.append(idade);
-    sexo=str(input('''Escolha o sexo
-                   [1] Feminino
-                   [2] Masculino
-                   [3] Outros
-                   [4] Prefiro não informar'''));
+    print('''[1] Feminino
+             [2] Masculino
+             [3] Outros
+             [4] Prefiro não informar ''');
+    sexo=int(input('Qual o sexo? '));
+    sexos.append(sexo);
+
+    if sexo==1:
+        if idade<20:
+            f_menor+=1;
 
     if sexo==2:
         if idade>mais_velho:
-            mais_valho=idade;
+            mais_velho=idade;
+            nome_homem=nome;
 
 media= sum(idades) / len(idades);
-print (f'A média de todas a idades é: {media}');
 
+print (f'A média de todas a idades é {media}');
+print(f'O mais velho dos homens é o {nome_homem} que tem {mais_velho} anos');
+print(f'Tem {f_menor} mulheres com menos de 20 anos');
