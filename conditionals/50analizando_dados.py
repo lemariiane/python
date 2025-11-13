c3b3b02ab2d43c35cb68768eb7aa3f247ae3dbec
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-12 23:52:59 -0300

    ok

--- conditionals/50analizando_dados.py
+++ conditionals/50analizando_dados.py
@@ -6,7 +6,7 @@ f_menor=0;
 for i in range(1,5):
     print(f'-------{i}° PESSOA-------');
     
-    nome=str(input('Digitte o nome: '));
+    nome=str(input('Digite o nome: '));
     idade=int(input('Digite a idade: '));
     idades.append(idade);
     print('''[1] Feminino \n[2] Masculino \n[3] Outros \n[4] Prefiro não informar''');
@@ -23,4 +23,4 @@ media= sum(idades) / len(idades);
 
 print (f'A média de todas a idades é {media}');
 print(f'O mais velho dos homens é o {nome_homem} que tem {mais_velho} anos');
-print(f'Tem {f_menor} mulheres com menos de 20 anos');
+print(f'Tem {f_menor} mulhere(s) com menos de 20 anos');
---
+++ conditionals/51jogo_adivinhacao.py
@@ -0,0 +1,24 @@
+#Jogo de adivinhação com quantidades de tentativas
+from random import randint;
+
+print("---Jogo de adinhação---");
+print("Pensei em número de 0 a 10");
+print("Você consegue adivinhar? E em quantos palpites?");
+
+acertou = False; 
+qntpalpite=0;
+numsortiado=randint(0,10);#computador pensando em um número
+
+while not acertou:
+    qntpalpite+=1;
+    palpite=int(input("Qual o seu palpite? "));
+    if numsortiado==palpite:
+        acertou=True;
+    else:
+        if palpite<numsortiado:
+            print('O número é MAIOR... Tente novamente');
+        else:
+            print('O número é MENOR... Tente novamente');
+
+
+print(f'PARABÉNS!!! Você acertou o número {numsortiado} em {qntpalpite} tentativa(s).')
