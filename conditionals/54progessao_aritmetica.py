ca7b35ca84ba4f0878ac4eda7f4fcff3566e56a0
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-18 00:10:35 -0300

    Update 54progessao_aritmetica.py

--- conditionals/54progessao_aritmetica.py
+++ conditionals/54progessao_aritmetica.py
@@ -2,4 +2,9 @@ print('GERADOR DE PA')
 
 num=int(input('Digite o primeiro termo: '))
 razao=int(input('Digite a razão da PA: '))
-
+i=0
+progressao=0
+while i<10:
+    progressao=num +i*razao
+    print(progressao, end=" ")
+    i+=1
