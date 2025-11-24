e26d2dc219ca8e44b7902daa3ac9acaee1479578
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-23 23:40:27 -0300

    ok

--- conditionals/56fibonacci.py
+++ conditionals/56fibonacci.py
@@ -1,16 +1,2 @@
 print('SEQUÊNCIA DE FIBONACCI')
-qnt=int(input('Deseja ver quantos termos? '))
-print('-'*30)
 
-t1=0 
-t2=1
-i=2
-
-print(t1, t2, end=' ')
-
-while i<qnt:
-    t3=t1+t2
-    t1=t2
-    t2=t3
-    print(t3, end=' ')
-    i+=1
