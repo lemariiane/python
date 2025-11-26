e0f1024d8d56ecbc6714e03dbaa3593ea27667fa
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-25 23:49:00 -0300

    Update 58maior_menor.py

--- conditionals/58maior_menor.py
+++ conditionals/58maior_menor.py
@@ -1,9 +1,9 @@
 continuar='S'
 soma=i=maior=menor=0
 
-while continuar in 'Ss':
+while continuar in 'S':
     num=int(input('Digite um número: '))
-    continuar=str(input('Quer contiuar? [S/N]: '))
+    continuar=str(input('Quer contiuar? [S/N]: ')).strip().upper()#ignora os espaços em branco na frente e coloca para maiúsculo
     soma+=num
 
     if i == 0:
@@ -15,6 +15,8 @@ while continuar in 'Ss':
     elif num<menor:
         menor=num
 
+    while continuar not in ('S', 'N'):
+        continuar = input('Valor inválido! Digite apenas S ou N: ').strip().upper()
 
 print(f'Você digitou {i} números e média foi {soma/i:.2f}')
 print(f'O maior valor é {maior} e o menor foi o {menor}')
