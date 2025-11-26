6f620570601943aa97de6dfc073ff4a03f4ad760
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-25 23:40:33 -0300

    Update 58maior_menor.py

--- conditionals/58maior_menor.py
+++ conditionals/58maior_menor.py
@@ -1,15 +1,20 @@
 continuar='S'
 soma=i=maior=menor=0
 
-while continuar=='S'or continuar=='s':
+while continuar in 'Ss':
     num=int(input('Digite um número: '))
     continuar=str(input('Quer contiuar? [S/N]: '))
     soma+=num
+
+    if i == 0:
+        maior = menor = num #maior e menor recebe o primeiro número digitado
+
     i+=1
     if num>maior:
         maior=num
-    elif num>menor:
+    elif num<menor:
         menor=num
 
-print(f'Você digitou {i} números e a soma é {soma} média foi {soma/i:.2f}')
+
+print(f'Você digitou {i} números e média foi {soma/i:.2f}')
 print(f'O maior valor é {maior} e o menor foi o {menor}')
