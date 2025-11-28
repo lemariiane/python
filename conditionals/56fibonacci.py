edeffe88938cc4bf166291a50d65f0ee2d6dac8b
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-11-27 23:45:09 -0300

    Update 56fibonacci.py

--- conditionals/56fibonacci.py
+++ conditionals/56fibonacci.py
@@ -1,2 +1,13 @@
 print('SEQUÊNCIA DE FIBONACCI')
+i=2 #pois já vamos mostrar os dois primeiros termos
+a=b=1
 
+qnt=int(input('Digite a quantidade de termos que deseja: '))
+print(a,b, end=' ')#primeiros termos sendo mostrados
+
+while qnt>i:
+    num=a+b
+    a=b #ir mudando a posição a cada loop
+    b=num #ir mudando a posição a cada loop
+    i+=1
+    print(num, end=' ')
