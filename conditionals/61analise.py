1e049411952f53793924e75bac287a43bec3c9a2
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-12-02 23:14:08 -0300

    Create 61analise.py

---
+++ conditionals/61analise.py
@@ -0,0 +1,31 @@
+maior_de_idade=qnt_homem=mulher_menor=0
+print('-'*30)
+print('CADASTRE UMA PESSOA')
+continua='x'
+while True:
+    print('-'*30)
+    idade=int(input('Idade: '))
+    print('''[1] Feminino \n[2] Masculino \n[3] Outros \n[4] Prefiro não informar''')
+    sexo=int(input('Qual o sexo? '))
+    print('-'*30)
+    continua=str(input('Deseja continuar? [S/N]')).strip().upper()
+
+    while True:
+        if continua in ('S','N'):
+            break
+        continua=str(input('DADO INVÁLIDO! Digite um valor válido: [S/N] ')).strip().upper()
+
+    if idade>18:
+        maior_de_idade+=1
+    if sexo==2:
+        qnt_homem+=1
+    if sexo==1 and idade<20:
+        mulher_menor+=1
+
+    if continua=='N':
+        break
+
+print('='*20)
+print(f'Total de pessoas com mais de 18 anos: {maior_de_idade}')
+print(f'Tootal de homens cadastrados: {qnt_homem}')
+print(f'Total de mulheres com menos de 20 anos: {mulher_menor}')
