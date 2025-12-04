aa7122033473ffb28cebacbd8c4b18c86848f53a
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-12-03 22:17:06 -0300

    Update 62loja.py

--- conditionals/62loja.py
+++ conditionals/62loja.py
@@ -8,15 +8,12 @@ while True:
     preco=float(input('Preço: '))
     continua=str(input('Deseja continuar? [S/N]')).strip().upper()
 
-    while True:
+    while True:#Validação de string
         if continua in ('S','N'):
             break
         continua=str(input('VALOR INVÁLIDO! Digite um valor válido! [S/N] ')).strip().upper()
 
     print('-'*30)
-
-    if qnt_produtos==0:
-        mais_barato=preco
 
     qnt_produtos+=1
     total+=preco
@@ -24,17 +21,18 @@ while True:
     if preco>1000:
         maior_mil+=1
 
-    if preco<mais_barato:
+    if qnt_produtos==0 or preco<mais_barato:#mais barato recebe o valor do preço no primeiro loop ou quando o preço for menor que o mais barato
         mais_barato=preco
         nome_barato=nome_produto
     
     if continua=='N':
         break
+
 if qnt_produtos==1:
     print(f'Tívemos {qnt_produtos} produto cadastrado.')
 else:
     print(f'Foram {qnt_produtos} produtos cadastrados.')
-    
+
 print(f'O total da compra foi: R${total}')
 print(f'Temos {maior_mil} produtos com o valor maior de R$1.000')
 print(f'O produto mais barato foi {nome_barato} que custa R${mais_barato}')
