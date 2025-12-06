2682fc88dccc6edab99a89477b4dc8cfcfc9d654
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-12-06 00:25:08 -0300

    Update 63caixa_eletronico.py

--- conditionals/63caixa_eletronico.py
+++ conditionals/63caixa_eletronico.py
@@ -2,32 +2,24 @@
 print('-'*30)
 print('CAIXA ELETRÔNICO')
 print('-'*30)
-total=sobrou=0
-cedula_um=0
-cedula_dez=0
-cedula_vinte=0
-cedula_cinquenta=0
 
-valor=int(input('Qual o valor que deseja sacar: R$'))
+# Lista das cédulas disponíveis
+cedulas = [50, 20, 10, 1] 
+contagem_cedulas = {} 
 
-if valor>50:
-    cedula_cinquenta=valor//50 #calcula a qauntidade de cédulas
-    restante=valor-(cedula_cinquenta*50) #calcula o retante para verficar se há necessidade de ter outras cédulas
-if restante>=20:
-    cedula_vinte=(restante//20)
-    restante-=(cedula_vinte*20)
-if restante>=10:
-    cedula_dez=(restante//10)
-    restante-=(cedula_dez*10)
-if restante>=1:
-    cedula_um=(restante//1)
+valor_saque = int(input('Qual o valor que deseja sacar: R$'))
+restante = valor_saque
 
-#só monstra para o usuário se ele receber ao menos 1 cedula do valor
-if cedula_cinquenta>1:
-    print(f'Total de {cedula_cinquenta} de R$50')
-if cedula_vinte>1:
-    print(f'Total de {cedula_vinte} de R$20')
-if cedula_dez>1:
-    print(f'Total de {cedula_dez} de R$10')
-if cedula_um>1:
-    print(f'Total de {cedula_um} de R$1')
+for cedula in cedulas:
+    if restante >= cedula:# Verifica se o valor restante é maior ou igual à cédula atual
+        quantidade = restante // cedula
+        restante = restante % cedula #atualiza o valor do restantate a cada loop
+        contagem_cedulas[cedula] = quantidade 
+
+        if quantidade > 0: #só aparece aquelas que o usuário irá receber
+            print(f'R${cedula:2d}: {quantidade} cédula(s)')
+            
+if restante > 0:
+    print(f'ATENÇÃO: R${restante} não pôde ser sacado.')
+
+print('-'*30)
