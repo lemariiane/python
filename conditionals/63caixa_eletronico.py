92a5ca7d70dd3da1a98cbbb0569828075c84a13d
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-12-07 23:53:27 -0300

    Update 63caixa_eletronico.py

--- conditionals/63caixa_eletronico.py
+++ conditionals/63caixa_eletronico.py
@@ -2,3 +2,24 @@
 print('-'*30)
 print('CAIXA ELETRÔNICO')
 print('-'*30)
+
+# Lista das cédulas disponíveis
+cedulas = [50, 20, 10, 1] 
+contagem_cedulas = {} 
+
+valor_saque = int(input('Qual o valor que deseja sacar: R$'))
+restante = valor_saque
+
+for cedula in cedulas:
+    if restante >= cedula:# Verifica se o valor restante é maior ou igual à cédula atual
+        quantidade = restante // cedula
+        restante = restante % cedula # Atualiza o valor do restantate a cada loop
+        contagem_cedulas[cedula] = quantidade 
+
+        if quantidade > 0: # Só aparece aquelas que o usuário irá receber
+            print(f'R${cedula:2d}: {quantidade} cédula(s)')
+            
+if restante > 0:
+    print(f'ATENÇÃO: R${restante} não pôde ser sacado.')
+
+print('-'*30)
