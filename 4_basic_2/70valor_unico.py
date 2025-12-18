24cfb0a98008106e440db009468c755b2849f6b4
lemariiane <129630136+lemariiane@users.noreply.github.com>
2025-12-17 22:14:18 -0300

    Create 70valor_unico.py

---
+++ 4_basic_2/70valor_unico.py
@@ -0,0 +1,21 @@
+lista=[]
+
+while True:
+    num=int(input('Digite um valor: '))
+    if num not in lista:
+        print('Valor adicionado com sucesso...')
+        lista.append(num)
+    else:
+        print('Não vou adicionar...')
+
+    continua=str(input('Quer continuar? [S/N]')).strip().upper()
+
+    while continua not in ('S','N'):
+        continua=str(input('Valor INVÁLIDO! Digite [S/N]: ')).strip().upper()
+        if continua in ('S','N'):
+            break
+    if continua in 'N':
+        break
+
+print('-'*30)
+print(lista)
