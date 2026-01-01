59f67e9b9e81f1ba31957d05bf271ca7285a3552
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-01 15:29:45 -0300

    Update 71lista_ordenada.py

--- 4_basic_2/71lista_ordenada.py
+++ 4_basic_2/71lista_ordenada.py
@@ -2,12 +2,15 @@ lista=[]
 
 for i in range (0,5):
     num=int(input('Digite um valor: '))
-    for numeros in lista:
-        if num>numeros:
+    if i ==0:
+        lista.append(num)
+    for n in lista:
+        if num>n:
             print('O valor foi adicionado no final da lista')
             lista.append(num)
-        elif num<numeros:
+        elif num<n:
             print('O valor foi adicionado na posição 0')
             lista.insert(0,num)
+print(lista)
 
 print('Volte sempre!!')
