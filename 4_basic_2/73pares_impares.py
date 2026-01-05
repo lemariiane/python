64d3b4f169d49565b7c562af4ce901aa4abc826b
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-05 20:07:09 -0300

    Update 73pares_impares.py

--- 4_basic_2/73pares_impares.py
+++ 4_basic_2/73pares_impares.py
@@ -4,9 +4,17 @@ par= list()
 impar=list()
 while True:
     num=int(input('Digite um número: '))
+    lista.append(num)
     cont=str(input('Deseja continuar? [S/N]')).strip().upper()
     while cont not in ('S','N'):
         cont=str(input('VALOR INVÁLIDO! Digite apenas S ou N: ')).strip().upper()
+    if num%2==0:
+        par.append(num)
+    else:
+        impar.append(num)
     if cont=='N':
         break
-print('cabou!')
+
+print(f'Os números são: {lista}')
+print(f'Os números pares são: {par}')
+print(f'Os números ímpares são: {impar}')
