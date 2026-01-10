495b9feb5c0469a647c8847e95a2e7c9c3860e57
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-09 21:58:55 -0300

    Create 77matriz.py

---
+++ 4_basic_2/77matriz.py
@@ -0,0 +1,13 @@
+#Gerando uma matriz 3X3
+matriz = []
+
+for l in range(0, 3):
+    linha = [] 
+    for c in range(0, 3):
+        num = int(input(f'Digite o valor para [{l}, {c}]: '))
+        linha.append(num) 
+    matriz.append(linha) 
+    
+print("-" * 30)
+for linha in matriz:
+    print(linha)
