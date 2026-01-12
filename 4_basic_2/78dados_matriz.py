71538c819ada16de216e743d10395e930c884e3b
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-12 19:57:09 -0300

    Update 78dados_matriz.py

--- 4_basic_2/78dados_matriz.py
+++ 4_basic_2/78dados_matriz.py
@@ -7,20 +7,20 @@ for l in range (0,3):
     for c in range (0,3):
         num=int(input(f'Digite o valor [{l}],[{c}]: '))
         if num %2==0:
-            soma_par=soma_par+num
-        linha.append(num)
-        if l==2:
-            soma_coluna=soma_coluna+num
-        if c==1:
-            if maior_linha<num:
-                maior_linha=num
-        
+            soma_par+=num
+        linha.append(num)     
     matriz.append(linha)
-    
-
 
 for linha in matriz:
     print(linha)
+    
+for l in range (0,3):
+    soma_coluna+=matriz[l][2]
+for c in range (0,3):
+    if c==0:
+        maior_linha=matriz[1][c]
+    elif matriz[1][c]>maior_linha:
+        maior_linha=matriz[1][c]
 
 print(f'A soma dos valores pares é {soma_par}')
 print(f'A soma dos valores da terceira coluna é {soma_coluna}')
