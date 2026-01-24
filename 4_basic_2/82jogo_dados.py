a70d932ffd3c64ec30cad712ae86b4c788f7585d
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-23 21:22:30 -0300

    Update 82jogo_dados.py

--- 4_basic_2/82jogo_dados.py
+++ 4_basic_2/82jogo_dados.py
@@ -1,5 +1,17 @@
 import random
+from operator import itemgetter
 
-for i in range(1,5):
-    dado=random.sample(range(1,6),1)
-    print(f'Jogador{i} tirou {dado} no dado')
+dicionario = {}
+
+for i in range(1, 5):
+    dado = random.randint(1, 6)
+    print(f'Jogador {i} tirou {dado} no dado')
+    dicionario[i] = dado
+
+print('=-' * 20)
+print('RANKING DOS JOGADORES')
+
+ranking = sorted(dicionario.items(), key=itemgetter(1), reverse=True)
+
+for i, v in enumerate(ranking):
+    print(f'{i+1}° lugar: jogador {v[0]} com {v[1]}')
