9301b4f9b4606488aa69932cd6b88b9b8a8e4dad
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-24 23:50:44 -0300

    Create 83trabalhador.py

---
+++ 4_basic_2/83trabalhador.py
@@ -0,0 +1,17 @@
+from datetime import date
+dicionario={}
+
+dicionario['nome']=str(input('Nome: '))
+dicionario['ano_nasc']=int(input('Ano de nascimento: '))
+dicionario['num_carteira']=int(input('Número da cateira de trabalho (digite 0 caso não tenha): '))
+
+ano_atual=date.today().year
+dicionario['idade']=ano_atual-dicionario['ano_nasc']
+
+if dicionario['num_carteira']!=0:
+    dicionario['ano_contratacao']=int(input('Ano de contratação: '))
+    dicionario['salario']=float(input('Salário: '))
+    dicionario['aposentadoria']=dicionario['idade']+50
+
+for k,v in dicionario.items():#k= key / v=value
+    print(f'{k} - {v} ')
