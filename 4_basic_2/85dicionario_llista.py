2bc5fa327aae43cd2a0fb2f7a204082c957c5139
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-27 01:10:59 -0300

    Update 85dicionario_llista.py

--- 4_basic_2/85dicionario_llista.py
+++ 4_basic_2/85dicionario_llista.py
@@ -1,6 +1,9 @@
+dicionario={}
 pessoas=[]
+nome_mulheres=[]
+idades=[]
+media=0
 while True:
-    dicionario={}
     dicionario['nome']=str(input('Nome: '))
     dicionario['sexo']=str(input('Sexo: [M/F] ')).strip().upper()
 
@@ -9,18 +12,29 @@ while True:
 
     dicionario['idade']=int(input('Idade: '))
 
+    idades.append(dicionario['idade'])
     pessoas.append(dicionario.copy())
 
     cont=str(input('Deseja continuar? [S/N] ')).strip().upper()
 
     while cont not in ('S','N'):
         cont=str(input('ERRO! Responda apenas S ou N: ')).strip().upper()
+    
+    if dicionario['sexo']=='F':
+        nome_mulheres.append(dicionario['nome'])
 
     if cont=='N':
         break
+
+media=(sum(idades))/len(pessoas)
+
 print('-='*30)
 print(f'Ao todo temos {len(pessoas)} pessoas cadastradas')
 print(f'A média das idades é {media}')
 print(f'As mulheres cadastradas foram {nome_mulheres}')
 print('Lista das pessoas que estão acima da média:')
-print(f'{pessoas_acima_media}')
+for i in pessoas:
+    if i['idade']>=media:
+        print('    ')
+        for k,v in i.items():
+            print(f'{k} = {v}')
