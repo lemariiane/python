089e2f6caa2421a28cbf25334fd9b1e7e7567be7
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-29 00:38:09 -0300

    Update 86jogador_apromorado.py

--- 4_basic_2/86jogador_apromorado.py
+++ 4_basic_2/86jogador_apromorado.py
@@ -15,7 +15,7 @@ while True:
     while cont not in ('S','N'):
         cont=str(input('DADO INVÁLIDO! Digite apenas S/N: ')).strip().upper()
     
-    dicionario['gols']=gols
+    dicionario['gols'] = gols[:]
     dicionario['total_gol']=sum(gols)
     dicionario['cod']=cod
 
@@ -31,9 +31,9 @@ print(f'{"cod":<4} {"nome":<15} {"gols":<15} {"total":<5}')
 print('-' * 60)
 for j in jogadores:
     print(f'{j["cod"]:<4} {j["nome"]:<15} {str(j["gols"]):<15} {j["total_gol"]:<5}')
-print('=-'*30)
 
 while True:
+    print('=-'*30)
     dado=int(input('Mostrar dados de qual jogador? (999 para interromper) '))
     if dado==999:
         break
@@ -42,7 +42,9 @@ while True:
         print('ERRO! Não existe um jogador com esse código.')
     
     else:
+        print('-'*30)
         print(f'LEVANTAMENTO DO JOGADOR {jogadores[dado]['nome']}')
+        print('-'*30)
         for jogos,g in enumerate(jogadores[dado]['gols']):
             print(f'No jogo {jogos+1} fez {g} gol(s)')
 
