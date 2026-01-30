1f21969cd940ddb4de3971cfb9dc665315fed416
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-01-29 23:30:13 -0300

    ok

---
+++ 4_basic_2/87terreno.py
@@ -0,0 +1,6 @@
+#Calculando uma área
+print('Controle de Terrenos')
+print('-'*15)
+larg=float(input('Largura (m): '))
+comp=float(input('Comprimento (m): '))
+print(f'A área do terreno é {larg*comp:.2f}m2.')
