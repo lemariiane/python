4d64d74c1f9cf4478ad5511b46aa19dc07f6cc83
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-02 18:23:49 -0300

    Update 90funcao_maior.py

--- 4_basic_2/90funcao_maior.py
+++ 4_basic_2/90funcao_maior.py
@@ -7,5 +7,7 @@ def descobrindo_maior(*num):
     print(f'Foram {total} valores ao todo')
     print(f'O maior valor informado foi {max(num)}')
 
+
 descobrindo_maior(2,9,4,5,7,1)
 descobrindo_maior(4,7,0)
+descobrindo_maior(0)
