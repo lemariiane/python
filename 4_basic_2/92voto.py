eef0aa4d812f714bfe15ea947772386f570b1af6
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-05 22:03:36 -0300

    Create 92voto.py

---
+++ 4_basic_2/92voto.py
@@ -0,0 +1,16 @@
+from datetime import date
+def voto(ano_nasc):
+    ano_atual=date.today().year
+    global idade
+    idade=ano_atual-ano_nasc
+    if idade<16:
+        return 'VOTO NEGADO'
+    elif (idade>=16 and idade<18) or (idade>70):
+        return 'VOTO FACULTATIVO'
+    else:
+        return 'VOTO OBRIGATÓRIO'
+
+
+ano_nasc=int(input('Em que ano você nasceu? '))
+status=voto(ano_nasc)
+print(f'Com {idade} anos: {status}')
