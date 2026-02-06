3123d675e1d93b16c2de699247904114a8af9e10
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-05 22:11:32 -0300

    Update 92voto.py

--- 4_basic_2/92voto.py
+++ 4_basic_2/92voto.py
@@ -1,5 +1,5 @@
-from datetime import date
 def voto(ano_nasc):
+    from datetime import date #só fica na memória durante a execução da def
     ano_atual=date.today().year
     global idade
     idade=ano_atual-ano_nasc
@@ -14,3 +14,4 @@ def voto(ano_nasc):
 ano_nasc=int(input('Em que ano você nasceu? '))
 status=voto(ano_nasc)
 print(f'Com {idade} anos: {status}')
+ 
