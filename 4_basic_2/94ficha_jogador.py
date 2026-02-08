b4932d1e7522d79ed211899e0571d3c90a634612
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-07 23:46:01 -0300

    Create 94ficha_jogador.py

---
+++ 4_basic_2/94ficha_jogador.py
@@ -0,0 +1,17 @@
+#Nome do jogador e seu número de gols no campeonato.
+#Com valores nulos sendo aceitos
+def jogador(nome=None,gols=None):
+    gols=gols if gols else 0
+    nome=nome if nome else '<desconhecido>'
+    return f'O jogador {nome} fez {gols} gol(s) no campeonato.'
+
+
+nome=str(input('Nome do jogador: ')).strip()
+gols=str(input('Número de gols: ')).strip()
+if not gols.isnumeric():
+    gols=None
+else:
+    gols=int(gols)
+
+
+print(jogador(nome,gols))
