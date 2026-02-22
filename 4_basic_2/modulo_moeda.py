b28e52f18a9495d3e786893d875c23c66e0ed9ce
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-22 10:29:44 -0300

    ok

--- 4_basic_2/modulo_moeda.py
+++ 4_basic_2/modulo_moeda.py
@@ -2,15 +2,15 @@
 #funções para calcular o preço
 def metade(preco= 0, formato=False):
     metade_preco=preco/2
-    return metade_preco if formato is False else moeda(preco)
+    return metade_preco if formato is False else moeda(metade_preco)
 
 def dobro(preco= 0, formato=False):
     dobro_preco=preco*2
-    return dobro_preco if formato is False else moeda(preco)
+    return dobro_preco if formato is False else moeda(dobro_preco)
 
 def aumento(preco= 0, formato=False):
     aumento_preco=(preco*0.1)+preco
-    return aumento_preco if formato is False else moeda(preco)
+    return aumento_preco if formato is False else moeda(aumento_preco)
 
 
 #formatação do valor 
