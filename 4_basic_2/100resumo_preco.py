7cda057ecef547312e08c543296059138f7c312f
lemariiane <129630136+lemariiane@users.noreply.github.com>
2026-02-22 22:02:49 -0300

    ok

---
+++ 4_basic_2/100resumo_preco.py
@@ -0,0 +1,6 @@
+import modulo_moeda as m
+
+preco=float(input('Digite o preço: R$'))
+taxaaum=float(input('Digite a porcentagem do aumento: '))
+taxared=float(input('Digite a porcentagem da redução: '))
+m.resumo(preco,taxaaum,taxared)
--- 4_basic_2/modulo_moeda.py
+++ 4_basic_2/modulo_moeda.py
@@ -1,5 +1,6 @@
-#módulo usado no exercício 99
-#funções para calcular o preço
+#módulo usado nos exercícios 99 e 100
+
+#funções para calcular o preço (exercício 99)
 def metade(preco= 0, formato=False):
     metade_preco=preco/2
     return metade_preco if formato is False else moeda(metade_preco)
@@ -8,9 +9,25 @@ def dobro(preco= 0, formato=False):
     dobro_preco=preco*2
     return dobro_preco if formato is False else moeda(dobro_preco)
 
-def aumento(preco= 0, formato=False):
-    aumento_preco=(preco*0.1)+preco
+def aumento(preco= 0, taxa=10, formato=False):
+    aumento_preco=preco+(preco*(taxa/100))
     return aumento_preco if formato is False else moeda(aumento_preco)
+
+def reducao(preco= 0, taxa=5, formato=False):
+    reducao_preco=preco-(preco*(taxa/100))
+    return reducao_preco if formato is False else moeda(reducao_preco)
+
+#resumo do preço (exercício 100)
+def resumo(preco=0, taxaaum=10, taxared=5):
+    print('=-'*30)
+    print('RESUMO DO VALOR'.center(20))
+    print('=-'*30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco,True)}')
+    print(f'Metade do preço: \t{metade(preco,True)}')
+    print(f'{taxaaum}% de aumento: \t{aumento(preco,taxaaum,True)}')
+    print(f'{taxared}% de redução: \t{reducao(preco,taxared,True)}')
+
 
 
 #formatação do valor 
